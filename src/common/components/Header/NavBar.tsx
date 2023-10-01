40f16606265ba586ba33a20a537382bbba27161e
BAYECHOU Abdelhadi <abdelhadi.bayechou@buawei.com>
2023-10-01 13:00:05 +0200

    [FEAT]: add  items to navbar

--- src/common/components/Header/NavBar.tsx
+++ src/common/components/Header/NavBar.tsx
@@ -1,19 +1,79 @@
 import MenuIcon from '@mui/icons-material/Menu';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import { useState } from 'react';
 import Logo from '../logo/Logo';
 import NavbarItem from './NavBarItem';
-
+import './Navbar.scss';
 export interface NavItem {
     id: string;
     href: string;
     text: string;
     dropdown: NavItem[];
     anchorEl: HTMLElement | null;
+}
+
+export default function PrimarySearchAppBar() {
+    const [navBarItems, setNavBarItems] = useState<NavItem[]>(links);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState<null | HTMLElement>(null);
+
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const handleMobileMenuClose = () => {
+        setMobileMoreAnchorEl(null);
+    };
+
+    const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+        setMobileMoreAnchorEl(event.currentTarget);
+    };
+
+    const toggleMenu = (index: number, el: HTMLElement | null) => {
+        console.log('toggle', index, el);
+        setNavBarItems((NavBarItems) =>
+            NavBarItems.map((menuItem, i) => (i === index ? { ...menuItem, anchorEl: el } : menuItem))
+        );
+    };
+
+    const mobileMenuId = 'primary-search-account-menu-mobile';
+    const renderMobileMenu = <></>;
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <div color="transparent" className="navbar">
+                <Toolbar
+                    className="tool-bar"
+                    sx={{ justifyContent: { xs: 'space-between', md: 'center' }, maxWidth: { md: '80%', xs: '100%' } }}
+                >
+                    <Logo />
+                    <Box sx={{ display: { xs: 'none', md: 'flex', justifyContent: 'space-between' } }}>
+                        {navBarItems.map((navBarItem, i) => (
+                            <NavbarItem
+                                key={navBarItem.id}
+                                toggleMenu={toggleMenu}
+                                index={i}
+                                navBarItem={navBarItem}
+                                type={'laptop'}
+                            />
+                        ))}
+                    </Box>
+                    <Box sx={{ display: { xs: 'flex', md: 'none' }, justifyContent: 'flex-end' }}>
+                        <IconButton
+                            size="large"
+                            aria-label="show more"
+                            aria-controls={mobileMenuId}
+                            aria-haspopup="true"
+                            onClick={handleMobileMenuOpen}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                    </Box>
+                </Toolbar>
+            </div>
+            {renderMobileMenu}
+        </Box>
+    );
 }
 
 const links: NavItem[] = [
@@ -90,62 +150,3 @@ const links: NavItem[] = [
         anchorEl: null,
     },
 ];
-
-export default function PrimarySearchAppBar() {
-    const [navBarItems, setNavBarItems] = useState<NavItem[]>(links);
-    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState<null | HTMLElement>(null);
-
-    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-    const handleMobileMenuClose = () => {
-        setMobileMoreAnchorEl(null);
-    };
-
-    const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-        setMobileMoreAnchorEl(event.currentTarget);
-    };
-
-    const toggleMenu = (index: number, el: HTMLElement | null) => {
-        console.log('toggle', index, el);
-        setNavBarItems((NavBarItems) =>
-            NavBarItems.map((menuItem, i) => (i === index ? { ...menuItem, anchorEl: el } : menuItem))
-        );
-    };
-
-    const mobileMenuId = 'primary-search-account-menu-mobile';
-    const renderMobileMenu = <></>;
-
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" color="transparent">
-                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <Logo />
-                    <Box sx={{ display: { xs: 'none', md: 'flex', justifyContent: 'space-between' }, maxWidth: '75%' }}>
-                        {navBarItems.map((navBarItem, i) => (
-                            <NavbarItem
-                                key={navBarItem.id}
-                                toggleMenu={toggleMenu}
-                                index={i}
-                                navBarItem={navBarItem}
-                                type={'laptop'}
-                            />
-                        ))}
-                    </Box>
-                    <Box sx={{ display: { xs: 'flex', md: 'none' }, justifyContent: 'flex-end' }}>
-                        <IconButton
-                            size="large"
-                            aria-label="show more"
-                            aria-controls={mobileMenuId}
-                            aria-haspopup="true"
-                            onClick={handleMobileMenuOpen}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                    </Box>
-                </Toolbar>
-            </AppBar>
-            {renderMobileMenu}
-        </Box>
-    );
-}
