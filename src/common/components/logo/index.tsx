bb706f96b2cbdbd4115cfa03bfe4a18034a3122e
BAYECHOU Abdelhadi <abdelhadi.bayechou@buawei.com>
2023-10-07 20:32:55 +0200

    [fix] : log img source

--- src/common/components/logo/index.tsx
+++ src/common/components/logo/index.tsx
@@ -1,30 +1,41 @@
-import { Link } from 'react-router-dom';
 import { Box } from '@mui/material';
+import { Link } from 'react-router-dom';
 import './Logo.scss';
 
 interface LogoProps {
     shrinkable?: boolean;
 }
 
-function Logo({shrinkable = false}: LogoProps) {
+function Logo({ shrinkable = false }: LogoProps) {
     return (
         <Link className="logo" to="/">
-            <Box component="img" src="./logo-vita-morocco.svg" alt="logo" sx={{
+            <Box
+                component="img"
+                src="/logo-vita-morocco.svg"
+                alt="logo"
+                sx={{
+                    display: shrinkable
+                        ? {
+                              xs: 'none',
+                              md: 'block',
+                          }
+                        : 'block',
+                }}
+            />
 
-                display:  shrinkable ? {
-                    xs: 'none',
-                    md: 'block',
-                } : 'block',
-            }} />
-
-            <Box component="img" src="./mini-logo-vita-morocco.svg" alt="logo" sx={{
-                display: shrinkable ? {
-                    xs: 'block',
-                    md: 'none',
-                } : 'none',
-            }} />
-        
-
+            <Box
+                component="img"
+                src="/mini-logo-vita-morocco.svg"
+                alt="logo"
+                sx={{
+                    display: shrinkable
+                        ? {
+                              xs: 'block',
+                              md: 'none',
+                          }
+                        : 'none',
+                }}
+            />
         </Link>
     );
 }
