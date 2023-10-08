3da7bc8f63c6c1bbc32881505126e0fdec5124cd
BAYECHOU Abdelhadi <abdelhadi.bayechou@buawei.com>
2023-10-08 13:15:13 +0200

    [fix]: social media colors

--- src/common/components/SocialMediaIcons/index.tsx
+++ src/common/components/SocialMediaIcons/index.tsx
@@ -1,21 +1,19 @@
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/FacebookOutlined';
-import './SocialMediaIcons.scss'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import './SocialMediaIcons.scss';
 
-interface ISocialMediaIcons{
-  displayColumn?: boolean,
-  float?: boolean
+interface ISocialMediaIcons {
+    displayColumn?: boolean;
+    float?: boolean;
 }
 
 export default function SocialMediaIcons({ displayColumn = false, float = false }: ISocialMediaIcons) {
-
-      
-  return (
-    <div className={`social-media-icons-container ${displayColumn && 'column'} ${float && 'float-container'}`}>
-        <InstagramIcon color="error" fontSize="large" className="icon instagram-icon"/>
-        <YouTubeIcon color="error" fontSize="large" className="icon youtube-icon"/>
-        <FacebookIcon color="primary" fontSize="large" className="icon facebook-icon"/>
-    </div>
-  )
+    return (
+        <div className={`social-media-icons-container ${displayColumn && 'column'} ${float && 'float-container'}`}>
+            <InstagramIcon color="info" fontSize="large" className="icon instagram-icon" />
+            <YouTubeIcon color="error" fontSize="large" className="icon youtube-icon" />
+            <FacebookIcon color="info" fontSize="large" className="icon facebook-icon" />
+        </div>
+    );
 }
