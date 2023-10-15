7b4b874a95b001ab15a6eb1637655ef19a078014
mustapha jaridi <mustaphajaridi@gmail.com>
2023-10-15 22:08:31 +0200

    [feature] : Home caorusel

--- src/common/components/Carousel/Carousel.tsx
+++ src/common/components/Carousel/Carousel.tsx
@@ -1,51 +1,160 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import Carousel from 'react-material-ui-carousel';
-import { Paper, Button, Typography } from '@mui/material';
-import './Carousel.scss';
+import { useState } from "react"
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import AdjustIcon from '@mui/icons-material/Adjust';
+import CircleIcon from '@mui/icons-material/Circle';
+import "./Carousel.scss"
 
-interface CarouselItem {
-    id: string;
-    name: string;
-    description: string;
-    imageUrl: string;
+type ImageSliderProps = {
+    images: {
+        id: string
+        name: string
+        description: string
+        url: string
+        alt: string
+    }[]
 }
 
-interface MyCarouselProps {
-    items: CarouselItem[];
-    autoSlide?: boolean;
-    slideInterval?: number;
-}
+export default function Carousel({ images }: ImageSliderProps) {
+    const [imageIndex, setImageIndex] = useState(0)
 
-const MyCarousel: React.FC<MyCarouselProps> = ({ items, autoSlide = true, slideInterval = 5000 }) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
+    function showNextImage() {
+        setImageIndex(index => {
+            if (index === images.length - 1) return 0
+            return index + 1
+        })
+    }
 
-    const handleNextClick = useCallback(
-        (now?: number | undefined) => now && setCurrentIndex(now)
-        , [])
+    function showPrevImage() {
+        setImageIndex(index => {
+            if (index === 0) return images.length - 1
+            return index - 1
+        })
+    }
 
     return (
-        <Carousel
-            index={currentIndex}
-            autoPlay={autoSlide}
-            animation="fade"
-            interval={slideInterval}
-            duration={500}
-            navButtonsProps={{ style: { backgroundColor: 'rgba(0, 0, 0, 0.3)' } }}
-            onChange={handleNextClick}
-            indicators={false}
-            strictIndexing
-            navButtonsAlwaysVisible
-            stopAutoPlayOnHover
+        <section
+            aria-label="Image Slider"
+            style={{ width: "100%", height: "100%", position: "relative" }}
         >
-            {items.map((item) => (
-                <div key={item.id} className="carousel-item">
-                    <img src={item.imageUrl} alt={item.name} />
-                    {/* <Typography variant="h6">{item.name}</Typography>
-                    <Typography variant="body2">{item.description}</Typography> */}
-                </div>
-            ))}
-        </Carousel>
-    );
-};
+            <a href="#after-image-slider-controls" className="skip-link">
+                Skip Image Slider Controls
+            </a>
+            <div
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    display: "flex",
+                    overflow: "hidden",
+                }}
+            >
+                {images.map(({ url, alt }, index) => (
+                    <img
+                        key={url}
+                        src={url}
+                        alt={alt}
+                        aria-hidden={imageIndex !== index}
+                        className="img-slider-img"
+                        style={{ translate: `${-100 * imageIndex}%` }}
+                    />
+                ))}
+            </div>
+            <button
+                onClick={showPrevImage}
+                className="img-slider-btn"
+                style={{ left: 0 }}
+                aria-label="View Previous Image"
+            >
+                <ArrowBackIosNewIcon aria-hidden />
+            </button>
+            <button
+                onClick={showNextImage}
+                className="img-slider-btn"
+                style={{ right: 0 }}
+                aria-label="View Next Image"
+            >
+                <ArrowForwardIosIcon aria-hidden />
+            </button>
+            <div
+                style={{
+                    position: "absolute",
+                    bottom: ".5rem",
+                    left: "50%",
+                    translate: "-50%",
+                    display: "flex",
+                    gap: ".25rem",
+                }}
+            >
+                {images.map((_, index) => (
+                    <button
+                        key={index}
+                        className="img-slider-dot-btn"
+                        aria-label={`View Image ${index + 1}`}
+                        onClick={() => setImageIndex(index)}
+                    >
+                        {index === imageIndex ? (
+                            <AdjustIcon aria-hidden />
+                        ) : (
+                            <CircleIcon aria-hidden />
+                        )}
+                    </button>
+                ))}
+            </div>
+            <div id="after-image-slider-controls" />
+        </section>
+    )
+}
 
-export default MyCarousel;
+
+
+// import React, { useState, useEffect, useCallback } from 'react';
+// import Carousel from 'react-material-ui-carousel';
+// import { Paper, Button, Typography } from '@mui/material';
+// import './Carousel.scss';
+
+// interface CarouselItem {
+//     id: string;
+//     name: string;
+//     description: string;
+//     imageUrl: string;
+// }
+
+// interface MyCarouselProps {
+//     items: CarouselItem[];
+//     autoSlide?: boolean;
+//     slideInterval?: number;
+// }
+
+// const MyCarousel: React.FC<MyCarouselProps> = ({ items, autoSlide = true, slideInterval = 5000 }) => {
+//     const [currentIndex, setCurrentIndex] = useState(0);
+
+//     const handleNextClick = useCallback(
+//         (now?: number | undefined) => now && setCurrentIndex(now)
+//         , [])
+
+//     return (
+//         <Carousel
+//             index={currentIndex}
+//             autoPlay={autoSlide}
+//             animation="fade"
+//             interval={slideInterval}
+//             duration={500}
+//             navButtonsProps={{ style: { backgroundColor: 'rgba(0, 0, 0, 0.3)' } }}
+//             onChange={handleNextClick}
+//             indicators={false}
+//             strictIndexing
+//             navButtonsAlwaysVisible
+//             stopAutoPlayOnHover
+//         >
+//             {items.map((item) => (
+//                 <div key={item.id} className="carousel-item">
+//                     <img src={item.imageUrl} alt={item.name} />
+//                     {/* <Typography variant="h6">{item.name}</Typography>
+//                     <Typography variant="body2">{item.description}</Typography> */}
+//                 </div>
+//             ))}
+//         </Carousel>
+//     );
+// };
+
+// export default MyCarousel;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,39 +1,57 @@
+import { Box } from '@mui/material';
 import Carousel from '../../common/components/Carousel';
 import { Page } from '../../common/components/Page/Page';
-import { useCounter } from '../../database/counter';
-import { useCounter as useCounterRedux } from '../../store/front/counter';
+// import { useCounter } from '../../database/counter';
+// import { useCounter as useCounterRedux } from '../../store/front/counter';
 
 
-const items = [
+const IMAGES = [
     {
         id: '444',
         name: 'Item 1',
         description: 'Description for Item 1',
-        imageUrl: 'https://phlearn.com/wp-content/uploads/2019/03/tom-grimbert-tomgrimbert-665112-unsplash.jpg?w=1600&quality=99&strip=all',
+        alt: 'Description for Item 1',
+        url: 'https://phlearn.com/wp-content/uploads/2019/03/tom-grimbert-tomgrimbert-665112-unsplash.jpg?w=1600&quality=99&strip=all',
     },
     {
         id: '445',
         name: 'Item 2',
         description: 'Description for Item 2',
-        imageUrl: 'https://images.unsplash.com/photo-1563635707628-9d39fd827e84?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
+        alt: 'Description for Item 2',
+        url: 'https://images.unsplash.com/photo-1563635707628-9d39fd827e84?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
     },
     {
         id: '4456',
         name: 'Item 3',
         description: 'Description for Item 3',
-        imageUrl: 'https://i.imgur.com/S1oE8qI.jpg',
+        alt: 'Description for Item 2',
+        url: 'https://i.imgur.com/S1oE8qI.jpg',
     },
 ];
 
 
 export const Home = (): JSX.Element => {
-    const { counter, handleDecrementClick, handleIncrementClick } = useCounter();
-    const { counter: c, handleDecrementClick: hDC, handleIncrementClick: hIC } = useCounterRedux();
+    // const { counter, handleDecrementClick, handleIncrementClick } = useCounter();
+    // const { counter: c, handleDecrementClick: hDC, handleIncrementClick: hIC } = useCounterRedux();
     const homeText = 'Home';
 
     return (
         <Page description={homeText} keywords={homeText} title={homeText}>
-            <Carousel autoSlide={true} slideInterval={10000} items={items} />
+            <Box
+                style={{
+                    maxWidth: "1200px",
+                    width: "100%",
+                    margin: "0 auto",
+                }}
+                sx={{
+                    aspectRatio: { xs: "9/6", md: "9/3" }
+                }}
+            >
+                <Carousel images={IMAGES} />
+                <a href="/" style={{ fontSize: "4rem" }}>
+                    Link
+                </a>
+            </Box>
         </Page>
     );
 };
