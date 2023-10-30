86aa806531c7d30bf11332d21470f1f380061c3a
BAYECHOU Abdelhadi <abdelhadi.bayechou@buawei.com>
2023-10-31 00:37:46 +0100

    feat: loading progress adjustements

--- src/common/components/Loading/Loading.tsx
+++ src/common/components/Loading/Loading.tsx
@@ -1,3 +1,7 @@
-import React from 'react';
-
-export const Loading = (): JSX.Element => <p>Loading...</p>;
+import { CircularProgress } from '@mui/material';
+import "./Loading.scss"
+export const Loading = (): JSX.Element => (
+    <div className="loading">
+        <CircularProgress />
+    </div>
+);
--- src/pages/Excursions/Excursions.tsx
+++ src/pages/Excursions/Excursions.tsx
@@ -2,17 +2,18 @@ import { useQuery } from 'react-query';
 import { Page } from '../../common/components/Page/Page';
 import { getExcursions } from '../../common/services/excursionService';
 import ExcursionSummary from './components/ExcursionSummary';
+import { CircularProgress } from '@mui/material';
 
 const ExcursionsText = 'Excursions';
 
 export const Excursions = (): JSX.Element => {
     const { data: excursions, isLoading } = useQuery('excursions', getExcursions);
 
-
     return (
         <Page description={ExcursionsText} keywords={ExcursionsText} title={ExcursionsText}>
             {isLoading ? (
-                <>Loading.........</>
+                // <>Loading.........</>
+                <CircularProgress />
             ) : (
                 <>
                     {excursions?.map((excursion) => (
