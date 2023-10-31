05117a00ff50a2c21437d9ff84859263abc190da
BAYECHOU Abdelhadi <abdelhadi.bayechou@buawei.com>
2023-10-31 01:09:01 +0100

    feat: add excursion card

--- src/pages/Excursions/Excursions.tsx
+++ src/pages/Excursions/Excursions.tsx
@@ -3,6 +3,7 @@ import { Page } from '../../common/components/Page/Page';
 import { getExcursions } from '../../common/services/excursionService';
 import ExcursionSummary from './components/ExcursionSummary';
 import { CircularProgress } from '@mui/material';
+import './Excursions.scss';
 
 const ExcursionsText = 'Excursions';
 
@@ -15,11 +16,47 @@ export const Excursions = (): JSX.Element => {
                 // <>Loading.........</>
                 <CircularProgress />
             ) : (
-                <>
+                <div className="excursions-container">
                     {excursions?.map((excursion) => (
-                        <ExcursionSummary key={excursion.id} />
+                        <ExcursionSummary key={excursion.id} excursion={excursion} />
                     ))}
-                </>
+                    {excursions?.map((excursion) => (
+                        <ExcursionSummary key={excursion.id} excursion={excursion} />
+                    ))}
+                    {excursions?.map((excursion) => (
+                        <ExcursionSummary key={excursion.id} excursion={excursion} />
+                    ))}
+                    {excursions?.map((excursion) => (
+                        <ExcursionSummary key={excursion.id} excursion={excursion} />
+                    ))}
+                    {excursions?.map((excursion) => (
+                        <ExcursionSummary key={excursion.id} excursion={excursion} />
+                    ))}
+                    {excursions?.map((excursion) => (
+                        <ExcursionSummary key={excursion.id} excursion={excursion} />
+                    ))}
+                    {excursions?.map((excursion) => (
+                        <ExcursionSummary key={excursion.id} excursion={excursion} />
+                    ))}
+                    {excursions?.map((excursion) => (
+                        <ExcursionSummary key={excursion.id} excursion={excursion} />
+                    ))}
+                    {excursions?.map((excursion) => (
+                        <ExcursionSummary key={excursion.id} excursion={excursion} />
+                    ))}
+                    {excursions?.map((excursion) => (
+                        <ExcursionSummary key={excursion.id} excursion={excursion} />
+                    ))}
+                    {excursions?.map((excursion) => (
+                        <ExcursionSummary key={excursion.id} excursion={excursion} />
+                    ))}
+                    {excursions?.map((excursion) => (
+                        <ExcursionSummary key={excursion.id} excursion={excursion} />
+                    ))}
+                    {excursions?.map((excursion) => (
+                        <ExcursionSummary key={excursion.id} excursion={excursion} />
+                    ))}
+                </div>
             )}
         </Page>
     );
--- src/pages/Excursions/components/ExcursionSummary.tsx
+++ src/pages/Excursions/components/ExcursionSummary.tsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router-dom';
+import { Excursion } from '../../../common/types/excursion';
+interface ExcursionSummaryProps {
+    excursion: Excursion;
+}
 
-export default function ExcursionSummary() {
-  return (
-    <div>ExcursionSummary</div>
-  )
+export default function ExcursionSummary({ excursion }: ExcursionSummaryProps) {
+    const navigate = useNavigate();
+    const goToExcursion = () => {
+        navigate(`./${excursion.id}`, { relative: 'path' });
+    };
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardHeader
+                action={
+                    <IconButton aria-label="view excursion" onClick={goToExcursion}>
+                        <VisibilityIcon />
+                    </IconButton>
+                }
+                title={excursion.title.fr}
+            />
+            <CardMedia
+                component="img"
+                height="194"
+                image="https://mui.com/static/images/cards/paella.jpg"
+                alt={`excursion ${excursion.title.fr}`}
+                onClick={goToExcursion}
+            />
+            <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                    {excursion.description.fr}
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                </IconButton>
+                <IconButton aria-label="share">
+                    <ShareIcon />
+                </IconButton>
+            </CardActions>
+        </Card>
+    );
 }
