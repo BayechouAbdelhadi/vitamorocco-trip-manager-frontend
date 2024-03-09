b90606f45e8d14bd4a1928015e4f74b07385646c
BAYECHOU Abdelhadi <abdelhadi.bayechou@buawei.com>
2024-03-09 23:43:41 +0100

    fix: responsivity tips

--- src/common/components/QuiltedImageList/TitleWithLines.tsx
+++ src/common/components/QuiltedImageList/TitleWithLines.tsx
@@ -7,18 +7,23 @@ interface TextWithLinesProps {
 function TextWithLines({ text }: TextWithLinesProps) {
     const isXs = useMediaQuery('(max-width:600px)');
     return (
-        <Box sx={{
-            width: { xs: '100%', md: '90%' },
-            margin: '3rem auto'
-        }}
+        <Box
+            sx={{
+                width: { xs: '100%', md: '90%' },
+                maxWidth: '100%',
+                margin: '3rem auto',
+            }}
         >
-            <Divider sx={{ borderBottomWidth: '45px' }} className="title-text-divider">
-                <Typography variant={isXs ? 'h4' : 'h3'} style={{
-                    "fontWeight": '600',
-                    "fontSize": "clamp(1.3rem, 2.3vw, 2rem)",
-                    "fontFamily": "Verdana, sans-serif",
-                    "position": "relative"
-                }}>
+            <Divider sx={{ borderBottomWidth: '45px', whiteSpace: 'pre-wrap' }} className="title-text-divider">
+                <Typography
+                    variant={isXs ? 'h4' : 'h3'}
+                    style={{
+                        fontWeight: '600',
+                        fontSize: 'clamp(1.3rem, 2.3vw, 2rem)',
+                        fontFamily: 'Verdana, sans-serif',
+                        position: 'relative',
+                    }}
+                >
                     {text}
                 </Typography>
             </Divider>
