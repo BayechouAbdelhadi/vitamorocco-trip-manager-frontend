4c17931b773fba9893dc2be11daa5ffd453a4766
Jaridi <mustapha.jaridi@capgemini.com>
2024-12-08 02:16:27 +0100

    remove unused imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,3 @@
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './common/components/App/App';
